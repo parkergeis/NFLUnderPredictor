58337f334c5f7de5f3ae78d42355c7512b7e1f53
Parker Geis <parkert.geis@icloud.com>
2024-08-23 13:15:07 -0400

    Video 2 updates
    Added pipelines

--- UnderPlays.py
+++ UnderPlays.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
+from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors, LocalOutlierFactor
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 import nfl_data_py as nfl
 import datetime as dt
 today = dt.date.today()
@@ -15,28 +17,17 @@ predWeek = currSeason[['week', 'total_line']].dropna()
 predWeek = predWeek.week.max()
 
 # Dataframe prep for modeling
-df = df[['home_team', 'away_team', 'season', 'total', 'game_type', 'week', 'gameday', 'gametime', 'location', 'away_moneyline', 'home_moneyline', 'spread_line', 'away_spread_odds', 'home_spread_odds', 'total_line', 'under_odds', 'over_odds', 'div_game', 'roof', 'surface', 'referee', 'stadium_id']]
+df = df[['home_team', 'away_team', 'season', 'total', 'week', 'gametime', 'spread_line', 'total_line', 'under_odds']]
 df['Over'] = np.where(df['total'] > df['total_line'], 1, 0)
 df['Under'] = np.where(df['total'] < df['total_line'], 1, 0)
 df['Push'] = np.where(df['total'] == df['total_line'], 1, 0)
 df = df[df.Push != 1]
 df.drop(columns='total', inplace=True)
-df['gameday'] = pd.to_datetime(df['gameday'])
-df['DayOfWeek'] = df['gameday'].dt.day_of_week
-# Function to convert time to seconds
-def time_to_seconds(time_str):
-    hours, minutes = map(int, time_str.split(':'))
-    return hours * 3600 + minutes * 60
-
-# Apply the function to the 'time' column
-df['gametime'] = df['gametime'].apply(time_to_seconds)
-df = pd.get_dummies(df, drop_first=True,columns=['game_type', 'location', 'roof', 'surface', 'referee', 'stadium_id'])
 df.reset_index(drop=True, inplace=True)
 df = df.dropna()
 
 # Model building
-feats = df.drop(columns=['home_team', 'away_team', 'season', 'gameday', 'Over', 'Under', 'Push'])
-features = feats.columns
+features = ['spread_line', 'total_line', 'under_odds']
 target = 'Under'
 
 train_df = df[(df.season < year) & (df.week < predWeek) | (df.season < year)]
@@ -46,9 +37,27 @@ y_train = train_df[target]
 X_test = test_df[features]
 y_test = test_df[target]
 
-model = KNeighborsClassifier(n_neighbors=13)
-classif = model.fit(X_train, y_train)
+pipe = Pipeline([
+    ('scaler', StandardScaler()),
+    ('model', KNeighborsClassifier(n_neighbors=7))
+])
+
+classif = pipe.fit(X_train, y_train)
+
+pipe2 = Pipeline([
+    ('scaler', StandardScaler()),
+    ('lof', LocalOutlierFactor(novelty=True))
+])
+
+pipe2.fit(X_train)
+y_test_nov = pipe2.predict(X_test)
+
+mask = [y == 1 for y in y_test_nov]
+
+X_test = X_test[mask]
+y_test = y_test[mask]
 y_pred = classif.predict(X_test)
+y_true = y_test
 
 # Predicted Plays log
 nextPlays = currSeason[currSeason.week == predWeek]
