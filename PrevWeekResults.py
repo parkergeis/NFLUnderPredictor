96724892330e0be820e2cfb6b37ce56298a5cd17
Parker Geis <parkert.geis@icloud.com>
2024-09-09 17:10:10 -0400

    Add 3 years for prev result

--- PrevWeekResults.py
+++ PrevWeekResults.py
@@ -5,9 +5,9 @@ today = dt.date.today()
 year = today.year
 
 # Variable declaration
-df = nfl.import_schedules(years=[year])
+df = nfl.import_schedules(years=range(year-3,year+1))
 # NFL Results log
-finalScores = df[['game_id', 'total', 'total_line', 'over_odds', 'under_odds', 'result', 'spread_line']].dropna()
+finalScores = df[['game_id', 'season', 'week', 'home_team', 'away_team', 'total', 'total_line', 'over_odds', 'under_odds', 'result', 'spread_line']].dropna()
 
 # Export to Sheets
 import gspread
@@ -16,4 +16,7 @@ sh = gc.open("Over/Under NFL Model")
 
 # Add weekly results
 worksheet2 = sh.worksheet("Results")
+total_rows = worksheet2.row_count
+if total_rows > 1:
+    worksheet2.batch_clear([f"A2:Z{total_rows}"])
 worksheet2.append_rows(finalScores.values.tolist())
